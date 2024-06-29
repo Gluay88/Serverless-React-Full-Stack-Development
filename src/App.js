e6adb4fa0a3b3a65f07990671013b7dbdb469d4e
gluay88 <p.wantagaew@gmail.comm>
2024-06-29 09:32:51 -0600

    Nav and base styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Game from "./pages/Game";
+import NavBar from "./components/NavBar";
+import { Main } from "./styled/Main";
+import { Container } from "./styled/Container";
+import Global from "./styled/Global";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+          <NavBar />
+          <Routes>
+            <Route path="/game" element={<Game />} />
+            <Route path="/highScores" element={<HighScores />} />
+            <Route path="/gameOver" element={<GameOver />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </Main>
+    </Router>
   );
 }
 
